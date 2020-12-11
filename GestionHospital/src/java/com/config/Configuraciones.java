4217048879d710df617f270aa19312dfa1785aac
kgbf <kevinflores99678@gmail.com>
2020-12-11 03:07:29 -0600

    controladores y paginas agregados

--- GestionHospital/src/java/com/config/Configuraciones.java
+++ GestionHospital/src/java/com/config/Configuraciones.java
@@ -1,6 +1,16 @@
 
 package com.config;
 
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 public class Configuraciones {
+   public DriverManagerDataSource dataSrc = new DriverManagerDataSource();
     
+    public DriverManagerDataSource conectar(){
+        dataSrc.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSrc.setUrl("jdbc:mysql://localhost:3306/hospitalbd");
+        dataSrc.setUsername("root");
+        dataSrc.setPassword("");
+        return dataSrc;
+    } 
 }
